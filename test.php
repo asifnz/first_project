95b45d4ad7f80ee6546fc4950bd4eed2f5b8dc36
asifnz <56282482+asifnz@users.noreply.github.com>
2019-10-08 13:52:31 +1300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Digital Technologies</title>
+	<style>
+		body{
+			 background-image: url("ai.jpg");
+ 			background-color: #cccccc;
+            background-repeat: no-repeat;
+            background-size: cover;
+		}
+		.container{
+			margin: auto;
+            margin-top: 100px;
+			width: 400px;
+			height:200px;
+			background-color: white;
+            border-radius: 5%;
+            padding: 50px;
+            font-weight: 700;
+            font-family: fantasy;
+            font-size: 30px;
+		}
+	</style>
+</head>
+
+<body>
+
+<div class="container">
+	<p align="center">
+		Digital Technologies Web Server
+	</p>
+</div>
+
+</body>
+</html>
